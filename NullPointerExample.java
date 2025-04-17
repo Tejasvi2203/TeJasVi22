34e06022ce0ff68c7f0aecd582728cfc85d4faa5
Tejasvi2203 <24chourasiat@rbunagpur.in>
2025-04-17 12:12:51 +0530

    NullPointerExample.java

---
+++ NullPointerExample.java
@@ -0,0 +1,11 @@
+
+public class NullPointerExample {
+    public static void main(String[] args) {
+        try {
+            String name = null;
+            System.out.println(name.length());  // calling method on null
+        } catch (NullPointerException e) {
+            System.out.println("NullPointer Exception caught: " + e.getMessage());
+        }
+    }
+}
