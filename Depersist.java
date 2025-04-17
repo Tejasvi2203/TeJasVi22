889757d5d931abc5face561f8f80ced8574f1099
Tejasvi2203 <24chourasiat@rbunagpur.in>
2025-04-17 12:09:43 +0530

    Depersist.java

---
+++ Depersist.java
@@ -0,0 +1,33 @@
+
+import java.io.*;
+
+class Student implements Serializable {  
+    int id;  
+    String name;  
+
+    public Student(int id, String name) {  
+        this.id = id;  
+        this.name = name;  
+    }  
+}
+
+class Depersist {
+    public static void main(String args[]) {
+        try {
+            // Creating stream to read the object
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("f.txt"));
+            Student s = (Student) in.readObject(); // Read the serialized object
+
+            // Printing the data of the deserialized object
+            System.out.println("Deserialization complete!");
+            System.out.println("Student ID: " + s.id);
+            System.out.println("Student Name: " + s.name);
+
+            // Closing the stream
+            in.close();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
