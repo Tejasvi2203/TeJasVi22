f45cc279918174ad6816bcad2d576e651413f5a9
Tejasvi2203 <24chourasiat@rbunagpur.in>
2025-04-17 12:11:34 +0530

    GrandParent.java

---
+++ GrandParent.java
@@ -0,0 +1,25 @@
+
+class Grandparent {
+    void displayGrandparent() {
+        System.out.println("I am the grandparent.");
+    }
+}
+
+class Parent extends Grandparent {
+    void displayParent() {
+        System.out.println("I am the parent.");
+    }
+}
+
+class Child extends Parent {
+    void displayChild() {
+        System.out.println("I am the child.");
+    }
+
+    public static void main(String[] args) {
+        Child obj = new Child();
+        obj.displayGrandparent();
+        obj.displayParent();
+        obj.displayChild();
+    }
+}
