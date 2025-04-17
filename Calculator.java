16a4243be2910145d8a80c60bad86dd6d84fbce1
Tejasvi2203 <24chourasiat@rbunagpur.in>
2025-04-17 12:05:18 +0530

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+
+public class Calculator {
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Overloaded method to add three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Overloaded method to add two doubles
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Add 2 ints: " + calc.add(10, 20));
+        System.out.println("Add 3 ints: " + calc.add(5, 15, 25));
+        System.out.println("Add 2 doubles: " + calc.add(3.5, 4.5));
+    }
+}
