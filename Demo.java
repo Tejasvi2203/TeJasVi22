9b7faa723560f9c5f545673e54e06faa90053d6c
Tejasvi2203 <24chourasiat@rbunagpur.in>
2025-04-17 12:08:23 +0530

    Demo.java

---
+++ Demo.java
@@ -0,0 +1,27 @@
+
+// First interface
+interface Printable {
+    void print();
+}
+
+// Second interface
+interface Showable {
+    void show();
+}
+
+// Class implementing both interfaces
+class Demo implements Printable, Showable {
+    public void print() {
+        System.out.println("Printing something...");
+    }
+
+    public void show() {
+        System.out.println("Showing something...");
+    }
+
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        obj.print();
+        obj.show();
+    }
+}
