de93d5f9351dba237e25e8286705e50414e30948
Tejasvi2203 <24chourasiat@rbunagpur.in>
2025-04-17 12:02:42 +0530

    ArrayIndexExample.java

---
+++ ArrayIndexExample.java
@@ -0,0 +1,11 @@
+
+public class ArrayIndexExample {
+    public static void main(String[] args) {
+        try {
+            int[] arr = {1, 2, 3};
+            System.out.println(arr[5]);  // invalid index
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBounds Exception caught: " + e.getMessage());
+        }
+    }
+}
